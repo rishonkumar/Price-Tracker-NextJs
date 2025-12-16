5a50eb6f5fa9e13de1d9d4c55c09751cdc717fd3
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-12-16 20:39:41 +0530

    Setting up the Landing screen added Form to add the Url of the product

---
+++ app/page.jsx
@@ -0,0 +1,77 @@
+import AddProductForm from "@/components/ui/AddProductFrom";
+import { Button } from "@/components/ui/button";
+import { Bell, Rabbit, Shield } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+
+  const user = null
+
+  const products = []
+  const FEATURES = [
+    {
+      icon: Rabbit,
+      title: "Lightning Fast",
+      description:
+        "Deal Drop extracts prices in seconds, handling JavaScript and dynamic content",
+    },
+    {
+      icon: Shield,
+      title: "Always Reliable",
+      description:
+        "Works across all major e-commerce sites with built-in anti-bot protection",
+    },
+    {
+      icon: Bell,
+      title: "Smart Alerts",
+      description: "Get notified instantly when prices drop below your target",
+    },
+  ];
+
+  return (
+    <main className="min-h-screen bg-linear-to-br from-orange-50 via-white to-orange-50">
+      <header className="bg-white/80 backdrop-blur-sm border-b border-gray-200 sticky top-0 z-10">
+        <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+          <div className="flex items- center gap-2">
+            <Image
+              className="h-10 w-auto"
+              src={"/deal-drop-logo.png"} alt="Logo" width={600} height={200} />
+          </div>
+
+          <Button variant="default" size="sm" className="bg-orange-500 hover:bg-orange-600 gap-2">Sign in</Button>
+        </div>
+      </header>
+
+      <section className="py-20 px-4">
+        <div className="max w-7xl mx-auto text-center">
+          <div className="inline-flex items-center gap-2 bg-orange-100 text-orange-700 px-6 py-2 rounded-full font-medium mb-6">
+            Made with Love by Rishon Kumar
+          </div>
+          <h2 className="text-5xl font-bold text-gray-900 mb-4 tracking-tight">Never miss a deal again</h2>
+          <p className="text-xl text-gray-600 mb-12 max-w-2xl mx-auto">Track prices of your favorite products and get notified when they drop below your target price.</p>
+
+          {/* ADd product form */}
+          <AddProductForm />
+
+
+          {/* Features */}
+          {products.length === 0 && (
+            <div className="grid md:grid-cols-3 gap-6 max-w-4xl mx-auto mt-16">
+              {FEATURES.map(({ icon: Icon, title, description }) => (
+                <div key={title}
+                  className="bg-white p-6 rounded-xl border border-gray-200"
+                >
+                  <div className="w-12 h-12 bg-orange-100 flex items-center justify-center mb-4 mx-auto">
+                    <Icon className="h-6 w-6 text-orange-500" />
+                  </div>
+                  <h3 className="font-semibold text-gray-900 mb-2">{title}</h3>
+                  <p className="text-sm text-gray-600">{description}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </main >
+  );
+}
---
+++ components/ui/AddProductFrom.jsx
@@ -0,0 +1,52 @@
+"use client"
+import { useState } from "react"
+import { Input } from "./input"
+import { Button } from "./button"
+import { Loader2 } from "lucide-react"
+
+const AddProductForm = () => {
+
+    const [url, setUrl] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        // setLoading(true)
+    }
+    return (
+        <form onSubmit={handleSubmit} className="w-full max-w-2xl mx-auto">
+            <div className="flex flex-col sm:flex-row gap-2">
+                <Input
+                    type="url"
+                    placeholder="Enter product URL"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                    className="h-12 text-base"
+                    required
+                    disabled={loading}
+                />
+
+                <Button
+                    type="submit"
+                    className="bg-orange-500 hover:bg-orange-600 h-10 sm:h-12 px-8"
+                    type="submit"
+                    disabled={loading}
+                    size={12}
+                >
+                    {loading ? (
+                        <>
+                            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                            Adding..
+                        </>
+                    ) : (
+                        "Track Price"
+                    )}
+                </Button>
+            </div>
+        </form >
+
+        // AuthModal
+    )
+}
+
+export default AddProductForm
